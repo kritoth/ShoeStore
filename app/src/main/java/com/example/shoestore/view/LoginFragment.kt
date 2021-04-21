3f0be03b823f094341f70225edb53171516935e6
kritoth <kritoth@gmail.com>
2021-04-21 12:24:22 +0200

    Implement navigation by setting up button clicklisteners: Login and Welcome

--- app/src/main/java/com/example/shoestore/view/LoginFragment.kt
+++ app/src/main/java/com/example/shoestore/view/LoginFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
 import com.example.shoestore.R
 import com.example.shoestore.databinding.FragmentLoginBinding
 
@@ -17,6 +18,13 @@ class LoginFragment : Fragment() {
 
         // Inflate the layout for this fragment
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
+
+        binding.btnLogin.setOnClickListener { findNavController()
+                .navigate(LoginFragmentDirections
+                        .actionLoginFragmentToOnboardWelcomeFragment())
+        }
+        binding.btnSignIn.setOnClickListener { findNavController()
+                .navigate(LoginFragmentDirections.actionLoginFragmentToOnboardWelcomeFragment())}
 
         return binding.root
     }
--- app/src/main/java/com/example/shoestore/view/OnboardWelcomeFragment.kt
+++ app/src/main/java/com/example/shoestore/view/OnboardWelcomeFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
 import com.example.shoestore.R
 import com.example.shoestore.databinding.FragmentOnboardWelcomeBinding
 
@@ -17,6 +18,12 @@ class OnboardWelcomeFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_onboard_welcome, container, false)
+
+        binding.btnNext.setOnClickListener { findNavController()
+                .navigate(OnboardWelcomeFragmentDirections
+                        .actionOnboardWelcomeFragmentToOnboardInstructionFragment3())
+        }
+
         return binding.root
     }
 
